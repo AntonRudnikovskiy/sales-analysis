adf7790a0153ded9dbbc8d1d587e8c83b0853747
Anton Rudnikovskiy <mkasadasa@yandex.ru>
2025-04-01 16:53:21 +0300

    init

---
+++ src/test/java/retail/expert/salesanalysis/SalesAnalysisApplicationTests.java
@@ -0,0 +1,13 @@
+package retail.expert.salesanalysis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesAnalysisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
